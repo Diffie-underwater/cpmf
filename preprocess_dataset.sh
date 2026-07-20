703798e66088d42f5410defdc3f478b968019800
Diffie-underwater <p4p.11.2026@gmail.com>
2026-07-20 12:49:12 +1200

    temp comment out other objects (working on ship)

--- preprocess_dataset.sh
+++ preprocess_dataset.sh
@@ -1,5 +1,5 @@
-data_dir=../../datasets/mvtec_3d
-save_dir=../../datasets/mvtec_3d_multiview
+data_dir=../../preparation/output
+save_dir=../../preparation/output-processed
 export DISPLAY=:1
 cd utils
 
@@ -8,14 +8,15 @@ python preprocessing.py --dataset_path $data_dir
 
 # note: if you run on a cluster without screen, you may need to complie headless open3d:
 # http://www.open3d.org/docs/release/tutorial/visualization/headless_rendering.html
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category bagel --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category cable_gland --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category carrot --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category cookie --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category dowel --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category foam --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category peach --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category potato --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category rope --save-dir $save_dir
-python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category tire --save-dir $save_dir
+python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category ship --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category bagel --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category cable_gland --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category carrot --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category cookie --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category dowel --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category foam --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category peach --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category potato --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category rope --save-dir $save_dir
+# python generate_multi_view_dataset.py --dataset_path $data_dir --color-option UNIFORM --category tire --save-dir $save_dir
 cd ..
